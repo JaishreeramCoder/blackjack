c5c7343af4228ee19b4a65bd44874f9a5f5873ec
JaishreeramCoder <sharmaadarsh859@gmail.com>
2025-03-18 21:30:44 +0530

    modified:   ppo/strategy-web-app/backend/backend/settings.py

--- ppo/strategy-web-app/backend/backend/settings.py
+++ ppo/strategy-web-app/backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
