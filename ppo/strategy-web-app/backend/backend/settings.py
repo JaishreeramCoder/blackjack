fb3c89f08a7fbf3c6a3157640cba4a78c0a75227
JaishreeramCoder <sharmaadarsh859@gmail.com>
2025-03-18 21:16:38 +0530

    Updated Django backend configuration: modified settings.py, synchronized package dependencies in requirements.txt, and adjusted production fetch request URL to ensure proper deployment readiness

--- ppo/strategy-web-app/backend/backend/settings.py
+++ ppo/strategy-web-app/backend/backend/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59a+qsfil^o&5wbmn+f#rjxy5m+a0jupcy-m*9s6g06ui95i&a'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
+import os
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 
 # Application definition
@@ -59,6 +61,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # or whatever origin your frontend uses
+    "https://blackjack-beta-kohl.vercel.app",
 ]
 
 
