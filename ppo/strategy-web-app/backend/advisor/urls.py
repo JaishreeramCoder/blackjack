308ef7d4d312f41757b4f7db273371c3e653fef5
JaishreeramCoder <sharmaadarsh859@gmail.com>
2025-03-23 19:07:00 +0530

    Removed Django backend folder since, fastapi on  hugging face space is used as backend

--- ppo/strategy-web-app/backend/advisor/urls.py
+++
@@ -1,8 +0,0 @@
-# advisor/urls.py
-from django.urls import path
-from .views import PredictBlackjackAPIView
-from .views import health
-urlpatterns = [
-    path('predict/', PredictBlackjackAPIView.as_view(), name='predict_blackjack'),
-    path('health/', health, name='health'),
-]
